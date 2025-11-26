3d1595939b758be4fcc1ba14b71e147b87841107
José Rincón <rincon.jmg@gmil.com>
2025-11-26 06:04:21 -0500

    add alert component

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,7 +1,30 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Alert Page</h1>
+    <div className="grid gap-3">
+      <Alert variant="default">
+        <Terminal />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+      <Alert variant="destructive">
+        <Terminal />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+      <Alert variant="success">
+        <Terminal color="green" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
