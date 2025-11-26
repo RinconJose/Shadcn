693d7b80aadfff87f41e35d672d45ca7014b2090
José Rincón <rincon.jmg@gmil.com>
2025-11-26 05:40:27 -0500

    add accordion component

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,43 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+const items = [
+  {
+    id: "item-1",
+    question: "Question 1",
+    answer: "Answer 1"
+  },
+  {
+    id: "item-2",
+    question: "Question 2",
+    answer: "Answer 2"
+  },
+  {
+    id: "item-3",
+    question: "Question 3",
+    answer: "Answer 3"
+  }
+];
+
 export default function Page() {
   return (
-    <div>
-      <h1>Accordion Page</h1>
+    <div><Accordion
+      type="single"
+      className="w-full"
+      defaultValue="item-1"
+    >
+      {
+        items.map( item => (
+          <AccordionItem value={item.id} key={item.id}>
+            <AccordionTrigger>{item.question}</AccordionTrigger>
+            <AccordionContent className="flex flex-col gap-4 text-balance">
+              <p>
+                {item.answer}
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+        ))
+      }
+    </Accordion>
     </div>
   );
 }
